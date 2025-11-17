2d292b8768c6fdc796f0c0b77727d240582d5114
sawyannaingwork-max <sawyannaingwork@gmail.com>
2025-11-17 17:37:34 +0630

    Intial set up

---
+++ animeworld/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors : {
+        background : "#0A0A0E",
+        text : "#fff",
+        primary : "#00E0FF",
+        secondary : "#FF3D8F"
+      },
+
+      fontFamily : {
+        itim : ["itim", "sans-serif"],
+        inter : ["inter", "sans-serif"],
+        roboto : ["roboto", "sans-serif"]
+      },
+
+      boxShadow : {
+        customShadow : "0px 0px 4px 2px #00e0ff"
+      }
+    },
+  },
+  plugins: [],
+}
+
